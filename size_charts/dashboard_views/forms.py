68e6ad587789dc670de0ca356eb7dadefdb62cd3
ryan28561 <ryan@lightningkite.com>
2018-10-04 15:39:07 -0600

    changed product form field to follow saleor views

--- size_charts/dashboard_views/forms.py
+++ size_charts/dashboard_views/forms.py
@@ -2,11 +2,16 @@ from django import forms
 from django.db.models import Q
 from django.utils.translation import pgettext_lazy
 
-from saleor.product.models import Category
+from saleor.product.models import Category, Product
 from ..models import SizeChart
 
+from django.urls import reverse_lazy
+from saleor.dashboard.forms import AjaxSelect2MultipleChoiceField
 
 class SizeChartForm(forms.ModelForm):
+    products = AjaxSelect2MultipleChoiceField(
+        queryset=Product.objects.all(),
+        fetch_data_url=reverse_lazy('dashboard:ajax-products'), required=False)
 
     class Meta:
         model = SizeChart
