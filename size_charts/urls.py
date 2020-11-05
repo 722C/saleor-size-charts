7c4d590d71b764efcaade651a16ba72c417469ce
ryan28561 <ryan@lightningkite.com>
2020-11-05 11:59:42 -0700

    allow no trailing slash

--- size_charts/urls.py
+++ size_charts/urls.py
@@ -3,16 +3,16 @@ from django.conf.urls import url
 from .dashboard_views import views as dashboard_views
 
 urlpatterns = [
-    url(r'^dashboard/size-charts/$',
+    url(r'^dashboard/size-charts/?$',
         dashboard_views.size_chart_list,
         name='size-chart-dashboard-list'),
-    url(r'^dashboard/size-charts/create/$',
+    url(r'^dashboard/size-charts/create/?$',
         dashboard_views.size_chart_create,
         name='size-chart-dashboard-create'),
-    url(r'^dashboard/size-charts/(?P<pk>[0-9]+)/$',
+    url(r'^dashboard/size-charts/(?P<pk>[0-9]+)/?$',
         dashboard_views.size_chart_details,
         name='size-chart-dashboard-detail'),
-    url(r'^dashboard/size-charts/(?P<pk>[0-9]+)/delete/$',
+    url(r'^dashboard/size-charts/(?P<pk>[0-9]+)/delete/?$',
         dashboard_views.size_chart_delete,
         name='size-chart-dashboard-delete'),
 ]
